5a3ddc73efb00aad5e053ebf208aee4adf0c2c52
danie <yytamab@connect.ust.hk>
2022-11-07 23:14:21 +0800

    testing features of map reduce in python

---
+++ giting.py
@@ -0,0 +1,8 @@
+import functools
+
+sample_list = [1,2,3,4]
+nested_list = [["Daniel",1],["Tom",2]]
+
+reduce_list = functools.reduce(lambda data,state: data + state , sample_list)
+map_list = tuple(map(lambda num: num[0], nested_list))
+print(map_list)
---
+++ yesing.sql
@@ -0,0 +1,5 @@
+select 
+* 
+from 
+student
+where something = 'something'
