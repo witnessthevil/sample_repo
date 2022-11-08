29425d47014c1fcd9696d12e67ff16ee2d1c5c40
danie <yytamab@connect.ust.hk>
2022-11-08 11:35:28 +0800

    the master change

--- giting.py
+++ giting.py
@@ -2,6 +2,7 @@ import functools
 
 sample_list = [1,2,3,4]
 nested_list = [["Daniel",1],["Tom",2]]
+the_other_example_list = ["Some name"]
 
 reduce_list = functools.reduce(lambda data,state: data + state , sample_list)
 map_list = tuple(map(lambda num: num[0], nested_list))
